2d0b0ff6db3ce401af1bf26b75c124a16ffa68c5
Jesper Vesterberg <jumziey@gmail.com>
2014-06-04 19:40:29 +0200

    Done! It's working as intended

--- serve.go
+++ serve.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"flag"
-	"net/http"
-	"log"
+	"fmt"
 	"io"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -13,21 +13,21 @@ var port = flag.String("p", "8080", "Sets server port")
 
 func createFileHandler(path string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w,r, path)
+		http.ServeFile(w, r, path)
 	}
 }
 
 func createFileIndex(files []string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		for _,v := range(files) {
-		io.WriteString(w, "<a href="+v+" download>"+v+"</a>\n")
+		for _, v := range files {
+			io.WriteString(w, "<a href="+v+" download>"+v+"</a><br>")
 		}
 	}
 }
 
 func fileExists(path string) bool {
 	_, err := os.Stat(path)
-	
+
 	if err == nil {
 		f, err := os.Open(path) //open
 		if err != nil {
@@ -35,12 +35,12 @@ func fileExists(path string) bool {
 			return false
 		}
 		defer f.Close()
-		fi, err := f.Stat()		//get file info
+		fi, err := f.Stat() //get file info
 		if err != nil {
 			fmt.Println(err)
 			return false
 		}
-		if !fi.IsDir() {	//is file?
+		if !fi.IsDir() { //is file?
 			return true
 		}
 	}
@@ -50,18 +50,19 @@ func fileExists(path string) bool {
 func main() {
 	flag.Parse()
 	var files []string
-	
-	for _,v := range(flag.Args()) {
-		if fileExists(v){
+
+	for _, v := range flag.Args() {
+		if fileExists(v) {
 			files = append(files, v)
-			http.HandleFunc(v, createFileHandler(v))
+			http.HandleFunc("/"+v, createFileHandler(v))
 		} else {
-		fmt.Println("Can't find the file: "+v,"\tSkipping...")
+			fmt.Println("Can't find the file: "+v, "\tSkipping...")
 		}
 	}
+	fmt.Print("Sharing: ")
 	fmt.Println(files)
-	fmt.Println("port: ", *port)
-	
+	fmt.Println("Server runs on port: ", *port)
+
 	fileIndex := createFileIndex(files)
 	http.HandleFunc("/", fileIndex)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
